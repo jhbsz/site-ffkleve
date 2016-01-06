cd57a1ef36a12465f22a85f4e3f502102e26a71b
Chrisno <c.nowak@nano-edv.de>
2016-01-06 11:17:58 +0100

    update site

--- site.mk
+++ site.mk
@@ -1,56 +1,37 @@
-##	gluon site.mk makefile example
-
-##	GLUON_SITE_PACKAGES
-#		specify gluon/openwrt packages to include here
-#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
-
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-alfred \
-	gluon-announced \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	haveged \
-	iptables \
-	iwinfo
+        gluon-mesh-batman-adv-14 \
+        gluon-alfred \
+        gluon-announced \
+        gluon-autoupdater \
+        gluon-setup-mode \
+        gluon-config-mode-core \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-hostname \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-contact-info \
+        gluon-config-mode-tunneldigger \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-luci-admin \
+        gluon-luci-autoupdater \
+        gluon-luci-portconfig \
+        gluon-luci-private-wifi \
+        gluon-luci-wifi-config \
+        gluon-luci-theme \
+        gluon-next-node \
+        gluon-mesh-vpn-tunneldigger \
+        gluon-radvd \
+        gluon-status-page \
+        gluon-migrate-vpn \
+        iwinfo \
+        iptables \
+        haveged
 
-##	DEFAULT_GLUON_RELEASE
-#		version string to use for images
-#		gluon relies on
-#			opkg compare-versions "$1" '>>' "$2"
-#		to decide if a version is newer or not.
+GLUON_LANGS ?= en de
 
-DEFAULT_GLUON_RELEASE := 0.7.2+exp+ffkle+$(shell date '+%Y%m%d')
-
-
-##	GLUON_RELEASE
-#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
-#		e.g.:
-#			$ make images GLUON_RELEASE=23.42+5
-#		would generate images named like this:
-#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+DEFAULT_GLUON_RELEASE := 0.7.3+exp+ffkle
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-# Default priority for updates.
 GLUON_PRIORITY ?= 0
-
-# Languages to include
-GLUON_LANGS ?= de en
