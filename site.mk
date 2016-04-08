7cee68257f25a116d8c8012e2fb9547a256e33b8
Chrisno <c.nowak@nano-edv.de>
2016-04-08 09:52:02 +0200

    Changes 2016.1.3

--- site.mk
+++ site.mk
@@ -1,7 +1,7 @@
 GLUON_SITE_PACKAGES := \
         gluon-mesh-batman-adv-14 \
         gluon-alfred \
-        gluon-announced \
+        gluon-respondd \
         gluon-autoupdater \
         gluon-setup-mode \
         gluon-config-mode-core \
@@ -23,13 +23,16 @@ GLUON_SITE_PACKAGES := \
         gluon-radvd \
         gluon-status-page \
         gluon-migrate-vpn \
+        gluon-ssid-changer \
+        gluon-tunneldigger-watchdog \
+	ffrg-txpower-fix \
         iwinfo \
         iptables \
         haveged
 
-GLUON_LANGS ?= en de
+GLUON_LANGS ?= de en
 
-DEFAULT_GLUON_RELEASE := 0.7.3+exp+ffkle
+DEFAULT_GLUON_RELEASE := 0.8.3+stable+ffkle
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
