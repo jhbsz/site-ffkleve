68a075f61660101da8fd003c0e39ea05483c2c55
Chrisno <c.nowak@nano-edv.de>
2016-04-11 15:20:29 +0200

    removed node02

--- site.mk
+++ site.mk
@@ -32,7 +32,7 @@ GLUON_SITE_PACKAGES := \
 
 GLUON_LANGS ?= de en
 
-DEFAULT_GLUON_RELEASE := 0.8.3+stable+ffkle
+DEFAULT_GLUON_RELEASE := 0.8.3.1+stable+ffkle
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
